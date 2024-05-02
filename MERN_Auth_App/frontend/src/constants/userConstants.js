ed7b2b2fe57a1dc0a3d3fd7607cb218cae3a7dda
Arshad <arshadk88@gmail.com>
2024-05-02 17:39:13 +0100

    first commit

---
+++ MERN_Auth_App/frontend/src/constants/userConstants.js
@@ -0,0 +1,7 @@
+export const USERS_URL = '/api/users'
+
+export const ACTION_INVALID = "This action is invalid!"
+export const FILE_TYPES = Object.freeze({
+  IMAGE: Object.freeze(["JPEG", "PNG"]),
+  VIDEO: Object.freeze(["MP4", "M4V", "MOV", "3GP", "3G2", "WMV", "ASF", "AVI", "FLV", "MKV", "WEBM"])
+})
